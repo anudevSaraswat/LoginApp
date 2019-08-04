1c19077d58dd93896218875338e7c087e17615fb
ANUDEV <mwanudev@gmail.com>
2019-08-04 17:04:52 +0530

    commit

--- app/src/main/java/com/example/loginapp/database/LoginDatabase.java
+++ app/src/main/java/com/example/loginapp/database/LoginDatabase.java
@@ -6,7 +6,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-
 public class LoginDatabase extends SQLiteOpenHelper {
 
     private SQLiteDatabase db;
